87e9dbc1c4160a3b10f53fba70fab79e2cb4bb8f
Christian Alvarenga <>
2023-03-27 12:38:55 -0400

    test

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Log;
 use App\Models\Payment;
 use Illuminate\Http\Request;
 
@@ -14,16 +15,24 @@ class WebhookController extends Controller
         $hmacExpected   = md5('adams' . $post . $secret);
         $hmacRecived    = $request->header('x-adams-notify-hash');
 
+        $log = new Log();
         $post = json_decode($post, true);
+        $log->data = json_encode(['notify' => $request->notify]);
+        $log->save();
 
         if ($hmacExpected !== $hmacRecived) return response()->json([], 401);
 
-        if ($post['notify']['type'] === 'debtStatus') {
+        if ($request->notify->type === 'debtStatus') {
             $docId              = $request->debt->docId;
             $payment            = Payment::where('doc_id', $docId)->get();
-            $payment->status    = $post['debt']['payStatus']['status'];
+            $payment->status    = $request->debt->payStatus->status;
             $payment->save();
+            $log->data = json_encode([$post, $payment]);
+        } else {
+            $log->data = json_encode([$post, $request->notify->type]);
         }
+
+        // $log->save();
 
         return response()->json([], 204);
     }
