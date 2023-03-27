e785900e9776125a9ce6208c874b13da5315b246
Christian Alvarenga <>
2023-03-27 12:34:13 -0400

    Changed the webhook controller logic

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Log;
 use App\Models\Payment;
 use Illuminate\Http\Request;
 
@@ -15,24 +14,16 @@ class WebhookController extends Controller
         $hmacExpected   = md5('adams' . $post . $secret);
         $hmacRecived    = $request->header('x-adams-notify-hash');
 
-        $log = new Log();
         $post = json_decode($post, true);
-        $log->data = json_encode($request->notify->type);
-        $log->save();
 
         if ($hmacExpected !== $hmacRecived) return response()->json([], 401);
 
-        if ($request->notify->type === 'debtStatus') {
+        if ($post['notify']['type'] === 'debtStatus') {
             $docId              = $request->debt->docId;
             $payment            = Payment::where('doc_id', $docId)->get();
-            $payment->status    = $request->debt->payStatus->status;
+            $payment->status    = $post['debt']['payStatus']['status'];
             $payment->save();
-            $log->data = json_encode([$post, $payment]);
-        } else {
-            $log->data = json_encode([$post, $request->notify->type]);
         }
-
-        $log->save();
 
         return response()->json([], 204);
     }
