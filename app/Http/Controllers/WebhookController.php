164d975fb8f5447ee216e9b7106113731bf49a82
Christian Alvarenga <>
2023-04-03 19:43:04 -0400

    test

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -15,8 +15,8 @@ class WebhookController extends Controller
         $hmacExpected   = md5('adams' . $post . $secret);
         $hmacRecived    = $request->header('x-adams-notify-hash');
 
-        $data = json_decode($request);
-        $data = json_encode($data);
+        $data = \json_decode($request->getContent(), true);
+        $data = \json_encode($data);
         $log = new Log();
         $log->data = $data;
         $log->save();
