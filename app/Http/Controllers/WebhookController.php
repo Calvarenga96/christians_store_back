256d21abe7a1884ac25b9ed4126cc18aa959b474
Christian Alvarenga <>
2023-04-03 20:32:43 -0400

    test

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -23,13 +23,16 @@ class WebhookController extends Controller
 
         if ($hmacExpected !== $hmacRecived) return response()->json([], 401);
 
-        if ($request['notify']['type'] === 'debtStatus') {
-            $docId              = $request['debt']['docId'];
-            $payment            = Payment::where('doc_id', $docId)->get();
-            $payment->status    = $request['debt']['payStatus']['status'];
-            $payment->save();
-        }
+        if ($request['notify']['type'] === 'debtStatus') $this->updateStatus($request);
 
         return response()->json([], 204);
+    }
+
+    public function updateStatus($request)
+    {
+        $docId              = $request['debt']['docId'];
+        $payment            = Payment::where('doc_id', $docId)->get();
+        $payment->status    = $request['debt']['payStatus']['status'];
+        $payment->save();
     }
 }
